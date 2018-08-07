a6d595fd14ec1f9b4320fcf23b5acd591e806f1c
downleave <downleave@qq.com>
2018-08-07 18:36:22 +0800

    修改详情页

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Learn Laravel 5</title>
+    <title>downleave's blog</title>
 
     <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <script src="//cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
