04ccb7c56904386793dac2145c099106ca5166e0
downleave <downleave@qq.com>
2018-11-20 15:17:12 +0800

    改变首页布局

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>downleave's blog</title>
-
-    <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <script src="//cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
-    <script src="//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-
-    <div id="title" style="text-align: center;">
-        <h1>downleave's blog</h1>
-        <div style="padding: 5px; font-size: 16px;">blog</div>
-    </div>
-    <hr>
+@section('content')
     <div id="content" class="col-md-10 col-md-offset-1">
         @foreach ($articles as $article)
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <a href="{{ url('article/'.$article->id) }}">
-                    <h4>{{ $article->title }}</h4>
-                </a>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <a href="{{ url('article/'.$article->id) }}">
+                        <h4>{{ $article->title }}</h4>
+                    </a>
+                </div>
+                <div class="panel-body">
+                    {!! $article->body !!}
+                </div>
             </div>
-            <div class="panel-body">
-                {!! $article->body !!}
-            </div>
-        </div>
         @endforeach
     </div>
     <div class="text-center">
         {{ $articles->links() }}
     </div>
-</body>
-</html>
+@endsection
